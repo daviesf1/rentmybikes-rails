d176f7b31001bbf26305429a8a607f4a6fb2a742
Dan Murphy <dan@boocx.com>
2013-08-12 14:19:58 -0700

    Add Subledger migrations

---
+++ db/migrate/20130812204106_add_subledger_ar_ap_ids.rb
@@ -0,0 +1,6 @@
+class AddSubledgerArApIds < ActiveRecord::Migration
+  def change
+    add_column :users, :subledger_ar_acct_id, :string
+    add_column :users, :subledger_ap_acct_id, :string
+  end
+end
