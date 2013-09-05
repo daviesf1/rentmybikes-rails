0647302cdfec10c71a60e16b378fad06daa01615
Alexandre Michetti Manduca <a.michetti@gmail.com>
2013-09-05 14:55:24 -0300

    forgot to push account route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 RentMyBike::Application.routes.draw do
   devise_for :users
 
-  resources :listings, :rentals, :reports
+  resources :listings, :rentals
+
+  get "/account" => "accounts#show"
 
   root 'listings#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
