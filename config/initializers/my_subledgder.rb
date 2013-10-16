603498d3c2adaf8a538445ddec76e46e141d2cfe
Alexandre Michetti Manduca <a.michetti@gmail.com>
2013-10-16 13:08:24 -0300

    update devise schema, remove copiled assets from github update creds

--- config/initializers/my_subledgder.rb
+++ config/initializers/my_subledgder.rb
@@ -1,9 +1,10 @@
 module MySubledger
   def self.new
-    Subledger.new :key_id  => ENV['SUBLEDGER_KEY_ID'],
-                  :secret  => ENV['SUBLEDGER_SECRET'],
-                  :org_id  => ENV['SUBLEDGER_ORG_ID'],
-                  :book_id => ENV['SUBLEDGER_BOOK_ID']
+    Subledger.new :key_id      => ENV['SUBLEDGER_KEY_ID'],
+                  :identity_id => ENV['SUBLEDGER_IDENTITY_ID'],
+                  :secret      => ENV['SUBLEDGER_SECRET'],
+                  :org_id      => ENV['SUBLEDGER_ORG_ID'],
+                  :book_id     => ENV['SUBLEDGER_BOOK_ID']
   end
 
   def self.escrow_account
